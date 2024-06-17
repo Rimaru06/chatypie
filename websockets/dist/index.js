409eea3d3632ca9fa0921c1298b39069c78a7613
Rimaru06 <shivraodanger04@gmail.com>
2024-06-17 16:59:07 +0530

    backend for one to one call

---
+++ websockets/dist/index.js
@@ -0,0 +1,37 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const http_1 = __importDefault(require("http"));
+const server = http_1.default.createServer((request, response) => {
+    console.log(new Date() + " Recieved request for" + request.url);
+    response.end("hi there");
+});
+const wss = new ws_1.WebSocketServer({ server });
+const clients = new Map();
+wss.on("connection", function connection(ws) {
+    ws.on("error", console.error);
+    const id = Math.random().toString(36).substring(2);
+    clients.set(id, ws);
+    ws.send(JSON.stringify({ id, type: "assign_id" }));
+    ws.on("message", (message) => {
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type == 'send_message') {
+            const { targetId, content } = parsedMessage;
+            const targetClient = clients.get(targetId);
+            if (targetClient) {
+                targetClient.send(JSON.stringify({ type: "message", content }));
+            }
+        }
+    });
+    ws.send("Hello! Message From Server!!");
+    ws.on('close', () => {
+        console.log("Client disconnected");
+        clients.delete(id);
+    });
+});
+server.listen(8080, () => {
+    console.log(new Date() + " Server is listening on port 8080");
+});
---
+++ websockets/src/index.ts
@@ -0,0 +1,42 @@
+import WebSocket, { WebSocketServer } from "ws";
+import http from "http";
+
+const server = http.createServer((request: any, response: any) => {
+  console.log(new Date() + " Recieved request for" + request.url);
+  response.end("hi there");
+});
+
+
+const wss = new WebSocketServer({ server });
+
+const clients = new Map();
+
+wss.on("connection", function connection(ws : WebSocket) {
+  ws.on("error", console.error);
+  const id = Math.random().toString(36).substring(2);
+  clients.set(id, ws);
+  ws.send(JSON.stringify({ id, type: "assign_id" }));
+
+  ws.on("message",  (message : any) => {
+    const parsedMessage = JSON.parse(message);
+    if(parsedMessage.type == 'send_message')
+        {
+            const {targetId , content} = parsedMessage;
+            const targetClient = clients.get(targetId);
+            if(targetClient)
+            {
+                targetClient.send(JSON.stringify({type: "message" , content}));
+            }
+        }
+  });
+   ws.send("Hello! Message From Server!!");
+
+   ws.on('close' , () => {
+    console.log("Client disconnected");
+    clients.delete(id);
+    });
+});
+
+server.listen(8080 ,  () => {
+    console.log(new Date() + " Server is listening on port 8080")
+})
